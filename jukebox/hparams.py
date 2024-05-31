f37367b55e79e97d6c6fcb6163eb78e8871017c8
arthurohman <89916399+arthurohman@users.noreply.github.com>
2024-05-31 15:53:36 +0200

    Update hparams.py

--- jukebox/hparams.py
+++ jukebox/hparams.py
@@ -571,7 +571,7 @@ DEFAULTS["distributed"] = Hyperparams(
     bucket=128
 )
 
-my_retrained_prior = Hyperparams(restore_prior='/content/drive/MyDrive/PLUGG/DT2119 Speaker Recognition/Project Jukebox/checkpoint_latest_1.pth.tar',
+my_retrained_prior = Hyperparams(restore_prior='/content/drive/MyDrive/PLUGG/DT2119 Speaker Recognition/Project Jukebox/Saved Models/checkpoint_latest_6501.pth.tar',
     level=2,
     labels=True,
     # TODO For the two lines below, if `--labels` was used and the model is
