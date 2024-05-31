0a6d12b79472006a3f4a33a1894546fdacf54742
dhurtigkth <100566590+dhurtigkth@users.noreply.github.com>
2024-05-31 16:55:37 +0200

    Update make_models.py

--- jukebox/make_models.py
+++ jukebox/make_models.py
@@ -63,7 +63,7 @@ def restore_model(hps, model, checkpoint_path):
             if checkpoint_hps.get(k, None) != hps.get(k, None):
                 print(k, "Checkpoint:", checkpoint_hps.get(k, None), "Ours:", hps.get(k, None))
         checkpoint['model'] = {k[7:] if k[:7] == 'module.' else k: v for k, v in checkpoint['model'].items()}
-        print(checkpoint)
+        #print(checkpoint['model'])
         model.load_state_dict(checkpoint['model'])
         if 'step' in checkpoint: model.step = checkpoint['step']
 
